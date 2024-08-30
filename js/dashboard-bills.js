02b1ac6370cfb484b1a86931aadb14885d8c1e84
Ivy Tran <ngocthuytien.ivy.tran@gmail.com>
2024-08-30 20:29:11 +1000

    Handle service's values changed
    Next TODO: deleting a single service on a bill

--- js/dashboard-bills.js
+++ js/dashboard-bills.js
@@ -3,6 +3,7 @@ import { Branches } from "../data/branch-list.js";
 import { serviceList } from "../data/service-list.js";
 import { Staff } from "../data/staff-list.js";
 import { Bill } from "./objects/bill.js";
+import { showDeleteAlert } from "../lib/components/dialog.js";
 
 document.querySelector("#bills").addEventListener('click', loadPage);
 window.onload = loadPage;
@@ -66,8 +67,18 @@ function handleListItemClicked(id){
     activateIndicator(id);
     
     //display bill details
-    document.querySelector('.col-2').innerHTML = currentBill.generateBillContent(tempServiceList, tempStaffList);
+    document.querySelector('.col-2').innerHTML = generateBillContent(currentBill);
     setStaffAndServices(currentBill.getBillServices());
+
+    //handle values changed
+    handleServiceOptionChanged(currentBill);
+    handleDiscountChanged(currentBill);
+    handleNoteChaged(currentBill);
+    handleStaffChanged(currentBill);
+
+    //handle buttons clicked
+    handleDeleteSingleService(currentBill);
+    document.querySelector('#check-out-btn').onclick = () => {console.log(currentBill)};
 
     //build the table
     new DataTable('#myTable', {
@@ -76,9 +87,148 @@ function handleListItemClicked(id){
         "info": false
     });
 }
+//modify the Price column and the Final Price column when a service on the bill is changed
+function handleServiceOptionChanged(billObj){
+    const serviceSelects = document.querySelectorAll('.table-select.service');
+    for(let i=0;i<serviceSelects.length;i++){
+        serviceSelects[i].onchange = (e) => {
+            //get the id of the bill service
+            const tableServiceId = e.target.id.slice(-1);
+            //get the service from the service list 
+            const service = serviceList.find((service)=>service.id == e.target.value);
+            const priceRow = document.querySelector(`#price-${tableServiceId}`);
+            priceRow.textContent = service.price;
+            const totalRow = document.querySelector(`#total-${tableServiceId}`);
+            const discountRow = document.querySelector(`#discount-${tableServiceId}`);
+            totalRow.textContent = service.price - discountRow.value; 
+            billObj.setServiceId(tableServiceId, parseInt(e.target.value));
+        }
+    }
+}
+//modify the Final Price column when discount is changed
+function handleDiscountChanged(billObj){
+    const discountInputs = document.querySelectorAll('.table-input.discount');
+    let numberRegex = /^\d+$/;
+    for(let i=0;i<discountInputs.length;i++){
+        discountInputs[i].onchange = (e) => {
+            //get the id of the bill service
+            const tableServiceId = e.target.id.slice(-1);
+            const priceRow = document.querySelector(`#price-${tableServiceId}`);
+            const totalRow = document.querySelector(`#total-${tableServiceId}`);
+            if(numberRegex.test(e.target.value) && parseInt(e.target.value) <= parseInt(priceRow.textContent)){
+                totalRow.textContent = parseInt(priceRow.textContent) - parseInt(e.target.value);
+                billObj.setServiceDiscount(tableServiceId, parseInt(e.target.value));
+            } else {
+                //e.target.value = "Discount must be a number or less than the original price";
+            }
+        }
+    }
+}
 
-function handleServiceInfoChanged(id){
+function handleStaffChanged(billObj){
+    const staffSelects = document.querySelectorAll('.table-select.staff');
+    for(let i=0;i<staffSelects.length;i++){
+        staffSelects[i].onchange = (e) => {
+            const tableServiceId = e.target.id.slice(-1);
+            billObj.setServiceStaff(tableServiceId, parseInt(e.target.value));
+        }
+    }
+}
 
+function handleNoteChaged(billObj){
+    const noteInputs = document.querySelectorAll('.table-input.note');
+    for(let i=0;i<noteInputs.length;i++){
+        noteInputs[i].onchange = (e) => {
+            const tableServiceId = e.target.id.slice(-1);
+            billObj.setServiceNote(tableServiceId, parseInt(e.target.value));
+        }
+    }
+}
+//TODO-Up to this point - deleting a service on the bill
+function handleDeleteSingleService(billObj){
+    const deleteSingleServiceBtns = document.querySelectorAll('.delete-table-btn');
+    const servicesListOnBill = billObj.getBillServices();
+    for(let i=0;i<deleteSingleServiceBtns.length;i++){
+        deleteSingleServiceBtns[i].onclick = () => {
+            servicesListOnBill.splice(deleteSingleServiceBtns[i].id, 1);
+            document.querySelector('#myTable').children[2].innerHTML = getRows(billObj);
+        }
+    }
+}
+
+//generate bill info to html
+function generateBillContent(billObj){
+    return `
+        <h2><b style="color: black">Bill ID:</b> ${billObj.getId()} | <b style="color: black">Client's name:</b> ${billObj.getCusName()} | <b style="color: black">Contact:</b> ${billObj.getMobile()} | <b style="color: black">Come at:</b> ${billObj.getEntranceDate()}, ${billObj.getEntranceTime()}</h2>
+        <table id="myTable" class="hover" style="width: 100%">
+            <thead>
+                <tr>
+                    <th>Service</th>
+                    <th>Price</th>
+                    <th>Done by</th>
+                    <th>Discount</th>
+                    <th>Final price</th>
+                    <th>Note</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                ${getRows(billObj)}
+            </tbody>
+        </table>
+        <div class="bill-footer">
+            <span style="margin-left: 20px">Total: <b style="color: #BBA366">$AU ${billObj.getTotal(tempServiceList)}</b> | Status: <b style="color: ${billObj.getStatus() ? "#4C7A6F" : "#C97C82"}">${billObj.getStatus() ? 'Paid' : 'Unpaid'}</b></span>
+            <div style="display: flex; margin-right: 20px; margin-bottom: 13px">
+                <button id="check-out-btn" class="square-btn confirm-btn">${billObj.getStatus() ? "Uncheck this bill" : "Check out"}</button>
+                <button id="delete-btn" class="square-btn cancel-btn">Delete</button>
+            </div>
+        </div>
+    `;
+}
+
+//get row content for the service table
+function getRows(billObj){
+    const serviceOptions = tempServiceList.map((service)=>{
+        return `<option value=${service.id}>${service.title}</option>`
+    }).join('');
+    const staffOptions = tempStaffList.map((staff)=>{
+        return `<option value=${staff.id}>${staff.firstName} ${staff.lastName}</option>`
+    }).join('');
+    let rowContent = billObj.getBillServices().map((item) => {
+        const service = serviceList.find((service)=>service.id == item.serviceId);
+        return `<tr>
+            <td><select id="service-${item.id}" class="table-select service" ${billObj.getStatus() ? "disabled" : ''}>${serviceOptions}</select></td>
+            <td id="price-${item.id}">${service.price}</td>
+            <td><select id="staff-${item.id}" class="table-select staff" ${billObj.getStatus() ? "disabled" : ''}>${staffOptions}</select></td>
+            <td><input class="table-input discount" type="text" name="bill-discount" id="discount-${item.id}" value='${item.discount == 0 ? "0" : item.discount}' placeholder="0" ${billObj.getStatus() ? "disabled" : ''}></td>
+            <td id="total-${item.id}">${parseInt(service.price) - parseInt(item.discount)}</td>
+            <td><input class="table-input note" type="text" name="bill-note" id="note-${item.id}" value='${item.note == "" ? "" : item.note}' placeholder="Enter something to describe this bill" ${billObj.getStatus() ? "disabled" : ''}></td>
+            <td>
+                <button id="${item.id}" class="round-btn delete-table-btn">
+                    <i class="fa-solid fa-trash fa-xs"></i> 
+                    <span class="tooltiptext">Delete</span>
+                </button> 
+            </td>
+        </tr>`
+    }).join('');
+    if(!billObj.getStatus())
+    {
+        rowContent += `<tr>
+            <td><select class="table-select service inactive">${serviceOptions}</select></td>
+            <td class="inactive">0</td>
+            <td><select class="table-select staff inactive">${staffOptions}</select></td>
+            <td><input class="table-input inactive" type="text" name="bill-discount" value="" placeholder="0"></td>
+            <td class="inactive">0</td>
+            <td><input class="table-input inactive" type="text" name="bill-note" id="bill-note" value="" placeholder="Enter something to describe this bill"></td>
+            <td>
+                <button class="round-btn sm add-table-btn">
+                    <i class="fa-solid fa-plus fa-xs"></i>
+                    <span class="tooltiptext">Add new</span> 
+                </button> 
+            </td>
+        </tr>`;
+    }
+    return rowContent;
 }
 
 //change choose indicator's color
--- js/objects/bill.js
+++ js/objects/bill.js
@@ -28,84 +28,32 @@ export class Bill{
         console.log(this.#id);
     }
 
-    //generate bill info to html
-    generateBillContent(serviceList, staffList){
-        //get current total of the bill
-        let total = this.#getTotal(serviceList);
-        return `
-            <h2><b style="color: black">Bill ID:</b> ${this.#id} | <b style="color: black">Client's name:</b> ${this.#cusName} | <b style="color: black">Contact:</b> ${this.#mobile} | <b style="color: black">Come at:</b> ${this.#entranceDate}, ${this.#entranceTime}</h2>
-            <table id="myTable" class="hover" style="width: 100%">
-                <thead>
-                    <tr>
-                        <th>Service</th>
-                        <th>Price</th>
-                        <th>Done by</th>
-                        <th>Discount</th>
-                        <th>Final price</th>
-                        <th>Note</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    ${this.getRows(serviceList, staffList)}
-                </tbody>
-            </table>
-            <div class="bill-footer">
-                <span style="margin-left: 20px">Total: <b style="color: #BBA366">$AU ${total}</b> | Status: <b style="color: ${this.#paid ? "#4C7A6F" : "#C97C82"}">${this.#paid ? 'Paid' : 'Unpaid'}</b></span>
-                <div style="display: flex; margin-right: 20px; margin-bottom: 13px">
-                    <button id="check-out-btn" class="square-btn confirm-btn">${this.#paid ? "Uncheck this bill" : "Check out"}</button>
-                    <button id="delete-btn" class="square-btn cancel-btn">Delete</button>
-                </div>
-            </div>
-        `;
+    setSingleService(serviceIdOnBill, newService){
+        const modifiedService = this.#services.find((item) => item.id == serviceIdOnBill);
+        modifiedService = newService;
     }
 
-    //get row content for the service table
-    getRows(serviceList, staffList){
-        const serviceOptions = serviceList.map((service)=>{
-            return `<option value=${service.id}>${service.title}</option>`
-        }).join('');
-        const staffOptions = staffList.map((staff)=>{
-            return `<option value=${staff.id}>${staff.firstName} ${staff.lastName}</option>`
-        }).join('');
-        let rowContent = this.#services.map((item) => {
-            const service = serviceList.find((service)=>service.id == item.serviceId);
-            return `<tr>
-                <td><select id=${item.id} class="table-select service" ${this.#paid ? "disabled" : ''}>${serviceOptions}</select></td>
-                <td>${service.price}</td>
-                <td><select id=${item.id} class="table-select staff" ${this.#paid ? "disabled" : ''}>${staffOptions}</select></td>
-                <td><input class="table-input" type="text" name="bill-discount" id="${item.id}" value='${item.discount == 0 ? "0" : item.discount}' placeholder="0" ${this.#paid ? "disabled" : ''}></td>
-                <td>${service.price - item.discount}</td>
-                <td><input class="table-input" type="text" name="bill-note" id="${item.id}" value='${item.note == "" ? "" : item.note}' placeholder="Enter something to describe this bill" ${this.#paid ? "disabled" : ''}></td>
-                <td>
-                    <button id="${item.id}" class="round-btn delete-table-btn">
-                        <i class="fa-solid fa-trash fa-xs"></i> 
-                        <span class="tooltiptext">Delete</span>
-                    </button> 
-                </td>
-            </tr>`
-        }).join('');
-        if(!this.#paid)
-        {
-            rowContent += `<tr>
-                <td><select class="table-select service inactive">${serviceOptions}</select></td>
-                <td class="inactive">0</td>
-                <td><select class="table-select staff inactive">${staffOptions}</select></td>
-                <td><input class="table-input inactive" type="text" name="bill-discount" value="" placeholder="0"></td>
-                <td class="inactive">0</td>
-                <td><input class="table-input inactive" type="text" name="bill-note" id="bill-note" value="" placeholder="Enter something to describe this bill"></td>
-                <td>
-                    <button class="round-btn sm add-table-btn">
-                        <i class="fa-solid fa-plus fa-xs"></i>
-                        <span class="tooltiptext">Add new</span> 
-                    </button> 
-                </td>
-            </tr>`;
-        }
-        return rowContent;
+    setServiceId(serviceIdOnBill, serviceId){
+        const modifiedService = this.#services.find((item) => item.id == serviceIdOnBill);
+        modifiedService.serviceId = serviceId;
     }
 
-    #getTotal(serviceList){
+    setServiceStaff(serviceIdOnBill, staffId){
+        const modifiedService = this.#services.find((item) => item.id == serviceIdOnBill);
+        modifiedService.staffId = staffId;
+    }
+
+    setServiceDiscount(serviceIdOnBill, discount){
+        const modifiedService = this.#services.find((item) => item.id == serviceIdOnBill);
+        modifiedService.discount = discount;
+    }
+
+    setServiceNote(serviceIdOnBill, note){
+        const modifiedService = this.#services.find((item) => item.id == serviceIdOnBill);
+        modifiedService.note = note;
+    }
+
+    getTotal(serviceList){
         let total = 0;
         this.#services.forEach((service)=> {
             const result = serviceList.find((item)=>item.id == service.serviceId);
@@ -118,8 +66,27 @@ export class Bill{
         return this.#services;
     }
 
-    setSingleService(serviceId, changedService){
-        const setService = this.#services.find((item) => item == serviceId);
-        setService = changedService;
+    getId(){
+        return this.#id;
+    }
+
+    getCusName(){
+        return this.#cusName;
+    }
+
+    getMobile(){
+        return this.#mobile;
+    }
+
+    getStatus(){
+        return this.#paid;
+    }
+
+    getEntranceDate(){
+        return this.#entranceDate;
+    }
+
+    getEntranceTime(){
+        return this.#entranceTime;
     }
 }
